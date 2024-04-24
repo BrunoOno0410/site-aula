2b4398f415365456da9d8883acc84f7c5d94a000
Aeon <admin@localhost.localdomain>
2024-04-24 18:38:34 -0300

    First development

---
+++ web/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const LandingPage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Site do TECNUS!</h1>
+      <p>Aprenda a ler e escrever de forma divertida e interativa.</p>
+      <button>Inscreva-se Agora</button>
+    </div>
+  );
+};
