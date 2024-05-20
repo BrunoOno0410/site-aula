edb41d467bf3a78caff8b149c1a167a229de29ab
BrunoOno0410 <brunogiordano.ono@gmail.com>
2024-05-20 07:30:29 -0300

    Home page refactored

--- web/src/pages/Home/Home.jsx
+++ web/src/pages/Home/Home.jsx
@@ -13,13 +13,13 @@ export const LandingPage = () => {
     <div>
       <Navbar />
       <div className="h-full">
-        <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+        <div className="relative isolate overflow-hidden bg-gray-900 h-dvh">
           <img
-            src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-            alt=""
+            src="..\src\assets\tech pattern.png"
+            alt="tech pattern"
             className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
           />
-          <div
+          {/* <div
             className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
             aria-hidden="true"
           >
@@ -42,30 +42,30 @@ export const LandingPage = () => {
                   "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
             />
-          </div>
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:mx-0">
-              <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Quem somos nós?
-              </h2>
-              <p className="mt-6 text-lg leading-8 text-gray-300">
-                Nosso compromisso com a educação de qualidade é refletido neste
-                projeto, que busca promover o letramento digital em regiões
-                vulneráveis. Capacitamos indivíduos através de programas de
-                formação de alta qualidade, conectando-os diretamente a grandes
-                empresas para acesso a empregos dignos e promovendo um
-                crescimento profissional significativo.
-              </p>
-            </div>
-            <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-              <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-                <div className="flex items-center">
-                  <h1>Oi</h1>
+          </div> */}
+          <div className="flex flex-col justify-center items-center h-full">
+            <div className="flex">
+              <div className="basis-2/3">
+                <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                  Quem somos nós?
+                </h2>
+                <p className="mt-6 text-lg leading-8 text-gray-300">
+                  Nosso compromisso com a educação de qualidade é refletido
+                  neste projeto, que busca promover o letramento digital em
+                  regiões vulneráveis. Capacitamos indivíduos através de
+                  programas de formação de alta qualidade, conectando-os
+                  diretamente a grandes empresas para acesso a empregos dignos e
+                  promovendo um crescimento profissional significativo.
+                </p>
+                <div className="">
+                  <div className="gap-x-8 gap-y-6 text-base font-semibold leading-7 text-white">
+                    <button>Saiba mais</button>
+                  </div>
                 </div>
               </div>
-              <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-                <h1>Teste</h1>
-              </dl>
+              <div className="basis-1/3 flex flex-col items-center justify-center h-full">
+                <img src="..\src\assets\LOGOVETO.svg" className=" " />
+              </div>
             </div>
           </div>
         </div>
